4a965798cfe2bf73b805ba7b856be21539756295
Ruben Samuel Gonzalez <55163305+WheatThinSam@users.noreply.github.com>
2020-12-01 19:28:39 -0600

    Update Test1.java

--- src/test/java/org/apache/commons/mail/Test1.java
+++ src/test/java/org/apache/commons/mail/Test1.java
@@ -6,17 +6,17 @@ import org.apache.commons.mail.SimpleEmail;
 
 import junit.framework.TestCase;
 
+
 public class Test1 extends TestCase{
 	Email testEmail;
 	public void setUp() {
 		testEmail = new SimpleEmail();
 	}
-	public void testAddBcc() throws EmailException {
+	public void testAddCc() throws EmailException {
 		System.out.println("This is a message from the test1 test class. pne***");
-		System.out.println("This Tests goal is to check for any errors while creating an email object and using multiple constructors.");
-    
-		testEmail.addBcc("a@b.com");
-		assertEquals("a@b.com", testEmail.getBccAddresses().get(0).toString());
+		System.out.println("This Tests goal is to check for any errors while using the AddCc method.");
+		testEmail.addCc("a@b.com");
+		assertEquals("a@b.com", testEmail.getCcAddresses().get(0).toString());
 	}
 
 }
