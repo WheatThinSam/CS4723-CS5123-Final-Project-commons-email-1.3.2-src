c403e80e07664c0f4171609d1d4eadc7da5c9bce
Ruben Samuel Gonzalez <55163305+WheatThinSam@users.noreply.github.com>
2020-12-01 19:21:39 -0600

    Create Test2.java

---
+++ src/test/java/org/apache/commons/mail/Test2.java
@@ -0,0 +1,23 @@
+  
+package org.apache.commons.mail;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import junit.framework.TestCase;
+
+public class Test2 extends TestCase{
+	Email testEmail;
+	public void setUp() {
+		testEmail = new SimpleEmail();
+	}
+	public void testAddBcc() throws EmailException {
+		System.out.println("This is a message from the test1 test class. pne***");
+		System.out.println("This Tests goal is to check for any errors while creating an email object and using multiple constructors.");
+    
+		testEmail.addBcc("eggs@burger.com");
+		assertEquals("eggs@burger.com", testEmail.getBccAddresses().get(0).toString());
+	}
+
+}
